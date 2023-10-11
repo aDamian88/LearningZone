f64b96b4bdf550d3406765ad349b3d402e4b664a
a.damianakis <alexandros.damianakis@external.toyotaconnected.eu>
2023-10-11 23:11:57 +0300

    Deletion of unusable mappers.

--- app/src/main/java/com/adamian/learningzone/domain/model/QuestionItem.kt
+++ app/src/main/java/com/adamian/learningzone/domain/model/QuestionItem.kt
@@ -1,35 +1,9 @@
 package com.adamian.learningzone.domain.model
 
-import com.adamian.learningzone.data.local.QuestionEntity
-
 
 data class QuestionItem(
     val question: String,
     val answerDescription: String,
     val options: List<String?>,
     val correctOption: String
-) {
-    companion object {
-        fun mapToEntity(questionItem: QuestionItem): QuestionEntity {
-            return QuestionEntity(
-                question = questionItem.question,
-                answerDescription = questionItem.answerDescription,
-                firstOption = questionItem.options[0],
-                secondOption = questionItem.options[1],
-                thirdOption = questionItem.options[2],
-                fourthOption = questionItem.options[3],
-                correctOption = questionItem.correctOption
-            )
-        }
-
-        fun mapToItem(questionEntity: QuestionEntity): QuestionItem {
-            return QuestionItem(
-                question = questionEntity.question ?: "",
-                answerDescription = questionEntity.answerDescription ?: "",
-                options = listOf(questionEntity.firstOption,questionEntity.secondOption,questionEntity.thirdOption,questionEntity.fourthOption),
-                correctOption = questionEntity.correctOption ?: ""
-
-            )
-        }
-    }
-}
+)
